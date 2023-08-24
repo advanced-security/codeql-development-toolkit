f944a7c172c118c077c09aa021efaaa6588d9c04
John L. Singleton <jsinglet@github.com>
2023-08-24 17:02:36 -0400

    fix

--- scripts/install-latest.sh
+++ scripts/install-latest.sh
@@ -16,6 +16,6 @@ unzip /opt/codeql-development-lifecycle-toolkit-downloads/qlt-linux-x86_64.zip -
 
 # ensure qlt is executable 
 chmod +x /opt/qlt/qlt
-
+chmod +r -R /opt/qlt/*
 # print the version that was installed 
 echo -e "\e[0;32m[QLT]\e[0m Installed QLT Version " `cat /opt/qlt/ver.txt`
