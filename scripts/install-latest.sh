201516c32e7a3add561aa40db21efa488bca4180
John L. Singleton <jsinglet@github.com>
2023-08-24 15:56:20 -0400

    added install script

---
+++ scripts/install-latest.sh
@@ -0,0 +1,21 @@
+#!/bin/env bash
+
+# create installation root directory 
+if [[ -d /opt/ ]] ; then
+    echo -e "\e[0;32m[QLT]\e[0m Skipping creation of installation directory..."
+else
+    echo -e "\e[0;32m[QLT]\e[0m Creating /opt" directory
+    mkdir -p /opt/ 
+fi
+
+# download the actual bundle 
+gh release download -R github/codeql-development-lifecycle-toolkit --pattern 'qlt-linux-x86_64.zip' -D /opt/codeql-development-lifecycle-toolkit-downloads
+
+# unzip the bundle
+unzip /opt/codeql-development-lifecycle-toolkit-downloads/qlt-linux-x86_64.zip -d /opt/qlt/
+
+# ensure qlt is executable 
+chmod +x /opt/qlt/qlt
+
+# print the version that was installed 
+echo -e "\e[0;32m[QLT]\e[0m Installed QLT Version " `cat /opt/qlt/ver.txt`
