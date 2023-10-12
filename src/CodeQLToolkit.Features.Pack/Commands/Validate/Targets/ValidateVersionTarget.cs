121dbe11f280e523a3964a49673a99ba1165ec5a
Jeongsoo Lee <jeongsoolee09@github.com>
2023-10-12 12:25:22 -0700

    Use CamlCaseNamingConvention by default with exception on default-suite-file

--- src/CodeQLToolkit.Features.Pack/Commands/Validate/Targets/ValidateVersionTarget.cs
+++ src/CodeQLToolkit.Features.Pack/Commands/Validate/Targets/ValidateVersionTarget.cs
@@ -1,13 +1,25 @@
 using CodeQLToolkit.Shared.Logging;
 using CodeQLToolkit.Shared.Utils;
 using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
 using Semver;
+using CodeQLToolkit.Features.Pack.Commands.Validate.Schemas;
 
 namespace CodeQLToolkit.Features.Pack.Commands.Validate.Targets
 {
+    public class YamlParseException : Exception
+    {
+        public string message { get; set; }
+        public YamlParseException(string failMessage)
+        {
+            message = failMessage;
+        }
+    }
     public class ValidateVersionTarget : CommandTarget
     {
         public string[] QlpackYmlFiles { get; set; }
+        private static Deserializer YamlDeserializer = (Deserializer)new DeserializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance).Build();
+
         public override void Run()
         {
             Console.WriteLine("Hello world! I got: ");
