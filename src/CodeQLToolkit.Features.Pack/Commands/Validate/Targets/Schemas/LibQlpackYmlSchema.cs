108297428c3df606c5ec4342e1014fcb5847764d
Jeongsoo Lee <jeongsoolee09@github.com>
2023-10-11 18:18:59 -0700

    Add Yaml file schemas to serve as deserialization targets

---
+++ src/CodeQLToolkit.Features.Pack/Commands/Validate/Targets/Schemas/LibQlpackYmlSchema.cs
@@ -0,0 +1,28 @@
+namespace CodeQLToolkit.Features.Pack.Commands.Validate.Schemas
+{
+    public class LibQlpackYmlFileSchema : IYmlSchema
+    {
+        public bool Library { get; set; }
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public string Suites { get; set; }
+        public string Extractor { get; set; }
+        public Dictionary<string, string> Dependencies { get; set; }
+
+        override public string ToString()
+        {
+            string DependenciesString = "\n";
+            if (Dependencies is not null)
+                foreach (KeyValuePair<string, string> pair in Dependencies)
+                    DependenciesString += $"{pair.Key}: {pair.Value}\n";
+
+            return $@"Test qlpack.yml file:
+                Library: {Library},
+                Name: {Name},
+                Version: {Version},
+                Suites: {Suites},
+                Extractor: {Extractor},
+                Dependencies: {DependenciesString}";
+        }
+    }
+}
---
+++ src/CodeQLToolkit.Features.Pack/Commands/Validate/Targets/Schemas/SrcQlpackYmlSchema.cs
@@ -0,0 +1,30 @@
+namespace CodeQLToolkit.Features.Pack.Commands.Validate.Schemas
+{
+    public class SrcQlpackYmlSchema : IYmlSchema
+    {
+        public bool Library { get; set; }
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public string Suites { get; set; }
+        public string Extractor { get; set; }
+        public Dictionary<string, string> Dependencies { get; set; }
+        public string DefaultSuiteFile { get; set; }
+
+        public override string ToString()
+        {
+            string DependenciesString = "\n";
+            if (Dependencies is not null)
+                foreach (KeyValuePair<string, string> pair in Dependencies)
+                    DependenciesString += $"{pair.Key}: {pair.Value}\n";
+
+            return $@"Test qlpack.yml file:
+                Library: {Library},
+                Name: {Name},
+                Version: {Version},
+                Suites: {Suites},
+                Extractor: {Extractor},
+                Dependencies: {DependenciesString},
+                DefaultSuiteFile: {DefaultSuiteFile}";
+        }
+    }
+}
