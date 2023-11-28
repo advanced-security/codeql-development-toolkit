1aabe16d8381c113139803b0d631068389110714
John L. Singleton <jsinglet@github.com>
2023-11-28 15:01:40 -0500

    removed backup

--- src_backup/src/CodeQLToolkit.Features.CodeQL.Lifecycle/Common.cs
+++
@@ -1,6 +0,0 @@
-﻿global using CodeQLToolkit.Shared.Target;
-global using CodeQLToolkit.Shared.Logging;
-global using Microsoft.Extensions.Logging;
-global using CodeQLToolkit.Shared.Feature;
-global using CodeQLToolkit.Shared.Options;
-
