bd6597da13260cf19a1feb1c3ece8d6693632e48
John L. Singleton <jsinglet@github.com>
2024-01-16 15:56:36 -0500

    adding test coverage back

---
+++ test/CodeQLToolkit.Shared.Tests/Utils/FileUtilsTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeQLToolkit.Shared.Tests.Utils
+{
+    public class FileUtilsTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+ 
+        [Test]
+        public void TestCreateTempDirectory()
+        {
+            var dir = FileUtils.CreateTempDirectory();           
+            Assert.IsTrue(Directory.Exists(dir));
+        }
+    }
+}
