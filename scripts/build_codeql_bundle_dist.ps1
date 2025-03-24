09c1604fae551c86a2a4b4686075b14956c293ea
Jeongsoo Lee <jeongsoolee09@github.com>
2025-03-24 08:37:33 -0400

    Update references to org and version
    - s/0.3.0/0.2.1/
    - s/kraiouchkine/advanced-security/

--- scripts/build_codeql_bundle_dist.ps1
+++ scripts/build_codeql_bundle_dist.ps1
@@ -20,7 +20,7 @@ if (-not (Test-Path $DestinationDirectory)) {
 }
 
 # download a copy of the release from GitHub
-gh release download "v$Version" --repo https://github.com/kraiouchkine/codeql-bundle  -D $WorkDirectory -A zip
+gh release download "v$Version" --repo https://github.com/advanced-security/codeql-bundle  -D $WorkDirectory -A zip
 
 # extract the zip file
 Expand-Archive -Path "$WorkDirectory\codeql-bundle-$Version.zip" -DestinationPath $WorkDirectory
