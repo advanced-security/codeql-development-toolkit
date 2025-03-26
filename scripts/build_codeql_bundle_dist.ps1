0512ddc1c5a590ebb241d53e6eeefce7dda32f71
Nicolas Will <nicolaswill@github.com>
2025-03-26 12:45:20 +0100

    Add error checks to build_codeql_bundle_dist

--- scripts/build_codeql_bundle_dist.ps1
+++ scripts/build_codeql_bundle_dist.ps1
@@ -1,15 +1,16 @@
 param(
     [Parameter(Mandatory = $true)] 
-    [string]
-    $Version,
-    [Parameter(Mandatory = $true)] 
-    [string]
-    $WorkDirectory,      
+    [string] $Version,
 
     [Parameter(Mandatory = $true)] 
-    [string]
-    $DestinationDirectory       
+    [string] $WorkDirectory,      
+
+    [Parameter(Mandatory = $true)] 
+    [string] $DestinationDirectory       
 )
+
+# Fail on any built-in command failure
+$ErrorActionPreference = "Stop"
 
 if (-not (Test-Path $WorkDirectory)) {
     New-Item -ItemType Directory -Path $WorkDirectory | Out-Null
@@ -19,33 +20,50 @@ if (-not (Test-Path $DestinationDirectory)) {
     New-Item -ItemType Directory -Path $DestinationDirectory | Out-Null
 }
 
-# download a copy of the release from GitHub
-gh release download "v$Version" --repo https://github.com/advanced-security/codeql-bundle  -D $WorkDirectory -A zip
+# Download a copy of the release from GitHub
+gh release download "v$Version" --repo https://github.com/advanced-security/codeql-bundle -D $WorkDirectory -A zip
+if ($LASTEXITCODE -ne 0) {
+    throw "Failed to download release from GitHub (gh)"
+}
 
-# extract the zip file
+# Extract the zip file
 Expand-Archive -Path "$WorkDirectory\codeql-bundle-$Version.zip" -DestinationPath $WorkDirectory
 
-# creates a directory named `codeql-bundle-<version>`
+# Create path to archive directory (named codeql-bundle-<version>)
 $ArchiveDirectory = Join-Path $WorkDirectory "codeql-bundle-$Version"
 
 Push-Location $ArchiveDirectory
 
-# at this point python should already be installed as well as poetry
-# export the requirements 
+# Export the requirements using poetry
 poetry self add poetry-plugin-export
-poetry export -f requirements.txt > requirements.txt
+if ($LASTEXITCODE -ne 0) {
+    throw "Failed to add poetry-plugin-export"
+}
 
-# install the requirements
+poetry export -f requirements.txt --output requirements.txt
+if ($LASTEXITCODE -ne 0) {
+    throw "Failed to export requirements using poetry"
+}
+
+# Install the requirements using pip
 pip install -r requirements.txt
+if ($LASTEXITCODE -ne 0) {
+    throw "Failed to install requirements using pip"
+}
 
+# Move into the cli directory
 Push-Location "codeql_bundle"
 
-# pyinstaller should also be installed  
+# Build executable with pyinstaller
 pyinstaller -F -n codeql_bundle cli.py
+if ($LASTEXITCODE -ne 0) {
+    throw "PyInstaller build failed"
+}
 
-Pop-Location 
-Pop-Location 
+Pop-Location
+Pop-Location
 
+# Determine built output binary path
 if ($IsWindows) {
     $OutputFile = Join-Path $ArchiveDirectory "codeql_bundle" "dist" "codeql_bundle.exe"
 }
@@ -53,8 +71,5 @@ else {
     $OutputFile = Join-Path $ArchiveDirectory "codeql_bundle" "dist" "codeql_bundle"
 }
 
-
-# this will output the binary in the `dist` directory - we should copy that binary the toplevel directory.
+# Copy the binary to the destination directory
 Copy-Item -Path $OutputFile -Destination $DestinationDirectory
-
-
